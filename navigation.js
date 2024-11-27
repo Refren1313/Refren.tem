d057989e2329edc1d41c4a5db5ab8adbd354be6c
Refren1313 <refren1313@gmail.com>
2024-11-27 21:10:23 +0600

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -13,6 +13,14 @@ const verify = document.querySelector('.verify');
 
 const main_page = document.querySelector('.main_page');
 
+const d_applications = document.querySelector('.d_applications');
+
+const w_applications = document.querySelector('.w_applications');
+
+const d_application_page = document.querySelector('.d_application_page');
+
+const w_application_page = document.querySelector('.w_application_page');
+
 const tasks_page = document.querySelector('.tasks_page');
 
 const task_page = document.querySelector('.task_page');
@@ -76,19 +84,55 @@ transfer.addEventListener('click', () => {
 
 const HELP = document.getElementById('H');
 HELP.addEventListener('click', () => {
- main_page.style.display = 'none';
- help_page.style.display = 'block';
+  main_page.style.display = 'none';
+  help_page.style.display = 'block';
 });
 const ABOUT = document.getElementById('A');
 ABOUT.addEventListener('click', () => {
- main_page.style.display = 'none';
- about_page.style.display = 'block';
+  main_page.style.display = 'none';
+  about_page.style.display = 'block';
+});
+
+const DApplications = document.getElementById('DApplications');
+DApplications.addEventListener('click', () => {
+  main_page.style.display = 'none';
+  d_applications.style.display = 'block';
+});
+
+const DAsBack = document.getElementById('DAsBack');
+DAsBack.addEventListener('click', () => {
+  d_applications.style.display = 'none';
+  main_page.style.display = 'block';
+});
+
+const WApplications = document.getElementById('WApplications');
+WApplications.addEventListener('click', () => {
+  main_page.style.display = 'none';
+  w_applications.style.display = 'block';
+});
+
+const WAsBack = document.getElementById('WAsBack');
+WAsBack.addEventListener('click', () => {
+  w_applications.style.display = 'none';
+  main_page.style.display = 'block';
+});
+
+const DapplicationBack = document.getElementById('DapplicationBack');
+DapplicationBack.addEventListener('click', () => {
+  d_application_page.style.display = 'none';
+  d_applications.style.display = 'block';
+});
+
+const WapplicationBack = document.getElementById('WapplicationBack');
+WapplicationBack.addEventListener('click', () => {
+  w_application_page.style.display = 'none';
+  w_applications.style.display = 'block';
 });
 
 const notificationBack = document.getElementById('notificationBack');
 notificationBack.addEventListener('click', () => {
-    notification_page.style.display = 'none';
-    main_page.style.display = 'block'
+  notification_page.style.display = 'none';
+  main_page.style.display = 'block'
 });
 
 
@@ -100,8 +144,8 @@ profileBack.addEventListener('click', () => {
 
 const tasksBack = document.getElementById('tasksBack');
 tasksBack.addEventListener('click', () => {
- tasks_page.style.display = 'none';
- main_page.style.display = 'block';
+  tasks_page.style.display = 'none';
+  main_page.style.display = 'block';
 });
 
 const dipBack = document.getElementById('dipBack');
@@ -130,24 +174,24 @@ ttfBack.addEventListener('click', () => {
 
 const pendingBack = document.getElementById('pendingBack');
 pendingBack.addEventListener('click', () => {
- task_page.style.display = 'none';
- main_page.style.display = 'block';
+  task_page.style.display = 'none';
+  main_page.style.display = 'block';
 });
 
 const helpBack = document.getElementById('helpBack');
 helpBack.addEventListener('click', () => {
- help_page.style.display = 'none';
- main_page.style.display = 'block';
+  help_page.style.display = 'none';
+  main_page.style.display = 'block';
 });
 
 const aboutBack = document.getElementById('aboutBack');
 aboutBack.addEventListener('click', () => {
- about_page.style.display = 'none';
- main_page.style.display = 'block';
+  about_page.style.display = 'none';
+  main_page.style.display = 'block';
 });
 
 const noPendingTaskBack = document.getElementById('noPendingTaskBack');
 noPendingTaskBack.addEventListener('click', () => {
- no_task_page.style.display = 'none';
- main_page.style.display = 'block';
+  no_task_page.style.display = 'none';
+  main_page.style.display = 'block';
 });
